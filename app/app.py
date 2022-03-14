5ea81e8d9530a6a197bb0af9e6f22b4182300228
Matt Ziminski <mzimin2@uic.edu>
2022-03-13 22:54:20 -0500

    add 'working' dockerized structure

---
+++ app/app.py
@@ -0,0 +1,58 @@
+# app.py
+from flask import Flask, render_template, request
+from typing import List, Dict
+import mysql.connector
+import json
+
+app = Flask(__name__, template_folder='templates', static_folder='static')
+# home route
+@app.route("/")
+def hello():
+    return "Hello, world!"
+    # return render_template('index.html', name = 'Jane', gender = 'Female')
+
+# serving form web page
+@app.route("/my-form")
+def form():
+    return render_template('form.html')
+
+# handling form data
+@app.route('/form-handler', methods=['POST'])
+def handle_data():
+    welcome_msg = 'Hello '
+    name = request.form['name']
+    
+    if request.form['gender'] == 'Male':
+        welcome_msg += 'Mr. ' + name
+    elif request.form['gender'] == 'Female':
+        welcome_msg += 'Mrs. ' + name
+
+    return welcome_msg
+
+
+# def getUsers() -> List[Dict]:
+#     config = {
+#         'user': 'root',
+#         'password': 'root',
+#         'host': 'db',
+#         'port': '3306',
+#         'database': 'user'
+#     }
+#     connection = mysql.connector.connect(**config)
+#     cursor = connection.cursor()
+#     cursor.execute('SELECT * FROM user')
+#     results = [{'user':username, 'email': email} for (username, _, email) in cursor]
+#     cursor.close()
+#     connection.close()
+
+#     return results
+
+# @app.route("/db")
+# def getDB():
+#     return  json.dumps({'Users': getUsers()})
+
+
+#Create the main driver function
+if __name__ == '__main__':
+    #call the run method
+    app.run(debug=True,host='0.0.0.0')
