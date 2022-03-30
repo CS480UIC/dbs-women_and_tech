65273cb7680151dd28259ae0d7bfc920a6412cdd
DiyinHu98 <hudiyin@gmail.com>
2022-03-30 12:20:26 -0500

    user table name change to member

--- sql/initializeDB.sql
+++ sql/initializeDB.sql
@@ -4,7 +4,7 @@ use women_tech;
 
 CREATE TABLE learning_resource
 (
-  Resource_ID int(5) NOT NULL primary key,
+  Resource_ID int(5) NOT NULL,
   Member_ID int(5) NOT NULL,
   Resource_Title VARCHAR(255) NOT NULL,
   Resource_Type VARCHAR(255) NOT NULL,
@@ -12,6 +12,9 @@ CREATE TABLE learning_resource
   Publisher VARCHAR(255) ,
   Publish_Year VARCHAR(255) NOT NULL,
   Resource_Language VARCHAR(255) NOT NULL
+  
+  PRIMARY KEY(Resource_ID),
+  FOREIGN KEY (Member_ID) REFERENCES member(member_id)
 );
 
 
@@ -21,5 +24,8 @@ CREATE TABLE network_event
   member_id int(5) NOT NULL,
   event_title VARCHAR(255) NOT NULL,
   event_address VARCHAR(255) NOT NULL,
-  event_date date NOT NULL
+  event_date date NOT NULL,
+  
+  PRIMARY KEY(event_id),
+  FOREIGN KEY (member_id) REFERENCES member(member_id)
 );
