e4aabb053ccf814e3576a27b3bb9ed32d0f76a3d
DiyinHu98 <hudiyin@gmail.com>
2022-03-30 12:15:48 -0500

    initialize table and upload csv

--- sql/initializeDB.sql
+++ sql/initializeDB.sql
@@ -1,9 +1,10 @@
--- Schemas name
-use tech;
+CREATE DATABASE women_tech;
 
-CREATE TABLE user
+use women_tech;
+
+CREATE TABLE learning_resource
 (
-  Resource_ID int(5) primary key,
+  Resource_ID int(5) NOT NULL primary key,
   Member_ID int(5) NOT NULL,
   Resource_Title VARCHAR(255) NOT NULL,
   Resource_Type VARCHAR(255) NOT NULL,
@@ -11,4 +12,14 @@ CREATE TABLE user
   Publisher VARCHAR(255) ,
   Publish_Year VARCHAR(255) NOT NULL,
   Resource_Language VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE network_event
+(
+  event_id int(5) NOT NULL primary key,
+  member_id int(5) NOT NULL,
+  event_title VARCHAR(255) NOT NULL,
+  event_address VARCHAR(255) NOT NULL,
+  event_date date NOT NULL
 );
