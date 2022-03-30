5cab8149ab3e215d075c79bd2df0684b8d8c9980
DiyinHu98 <hudiyin@gmail.com>
2022-03-30 14:17:12 -0500

    add simple and aggregate queries

---
+++ sql/queries.sql
@@ -0,0 +1,26 @@
+use tech;
+
+-- Simple Queries
+
+select *
+from learning_resource
+where Resource_Type = 'Book'
+order by Member_ID
+;
+
+select event_id, event_title
+from network_event
+where member_id = 1
+;
+
+-- Aggregate Queries
+
+select Member_ID, count(*) as total_resourse
+from learning_resource
+group by Member_ID
+;
+
+
+select event_title, event_address, date(event_date) as date_time
+from network_event
+;
