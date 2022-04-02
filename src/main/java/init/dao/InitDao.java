50257e83cb99d06685b27a91c70e5ccf4cf5b589
Matt Ziminski <mzimin2@uic.edu>
2022-04-01 21:35:41 -0500

    Implement Initalize DB to webpage

---
+++ src/main/java/init/dao/InitDao.java
@@ -0,0 +1,40 @@
+package init.dao;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.apache.ibatis.jdbc.ScriptRunner;
+
+
+
+/**
+ * DDL functions performed in database
+ */
+public class InitDao {
+	
+
+	public void initialize(InputStream input) throws ClassNotFoundException {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/women_in_tech","womenTech", "Uic1234567!");
+			
+
+			//Initialize the script runner
+	   		 ScriptRunner sr = new ScriptRunner(connect);
+	   		 //Creating a reader object
+	   		 Reader reader = new InputStreamReader(input);
+	   		 //Running the script
+	   		 sr.runScript(reader);
+		    
+		    connect.close();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+		
+}
